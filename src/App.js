2e70385b4d2a08d4e41a54649d0fbd0b13c78273
jmirav <jmira420@gmail.com>
2018-08-16 17:09:50 -0500

    Markup final version

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Container, Button, Image, Icon, Divider, Header, Segment, Grid, Rail } from 'semantic-ui-react';
+import { Container, Button, Image, Icon, Header, Grid } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <Container textAlign="justified">
-        <Header as='h1' textAlign='center' dividing>
-          Blog posts populares
-        </Header>
-        
-        <div>
-          <p>Orden: </p>
-          <Button content='Ascendente' primary inline />
+        <div className='title'>
+          <Header as='h1' textAlign='center' dividing>
+            Blog posts populares
+          </Header>
+        </div>
+
+        <div className='sorting'>
+          <Header as='h3' inline>
+            Orden:
+          </Header>
           <Button basic color='blue'>Ascendente</Button>
           <Button content='Descendente' primary />
         </div>
 
-        <Grid centered columns={3}>
-          <Grid.Column width={6}>
+        <Grid centered columns={3} >
+          <Grid.Column width={6} className="post">
             <Image src='http://blog.makeitreal.camp/assets/images/bg-images/bundler.jpg' size='large' />
           </Grid.Column>
-          <Grid.Column textAlign='center' width={1}>
+          <Grid.Column textAlign='center' width={1} className="post" verticalAlign="middle">
             <Icon name='angle up' />
-            <p>42</p>
+            <Header as='h3'>42</Header>
             <Icon name='angle down' />
 
           </Grid.Column>
-          <Grid.Column width={9}>
-            <h3>Manejo de dependencias en Ruby con Bundler</h3>
+          <Grid.Column width={9} className="post">
+            <a href="http://blog.makeitreal.camp/manejo-de-dependencias-en-ruby-con-bundler/" target='_blank' rel="noopener noreferrer">
+              <Header as='h3' color='blue'>
+                Manejo de dependencias en Ruby con Bundler
+              </Header>
+            </a>
             <p>Bundler es una manejador de dependencias para Ruby. Aunque viene incluido con Rails, Bundler no es exclusivo de Rails, lo puedes usar para manejar las dependencias de cualquier proyecto de Ruby.</p>
-            <p>Escrito por: <Image src='//a.disquscdn.com/uploads/users/2864/1155/avatar92.jpg?1481303405' size='mini' circular inline /></p>
+            <p className="author">Escrito por: <Image src='//a.disquscdn.com/uploads/users/2864/1155/avatar92.jpg?1481303405' size='mini' circular inline /></p>
           </Grid.Column>
         </Grid>
       </Container>
