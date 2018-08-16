9bb35ddcea4a134a3d3ef099dead655f4c5f7b4a
jmirav <jmira420@gmail.com>
2018-08-15 20:30:04 -0500

    Adjust first version of markup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Container } from 'semantic-ui-react';
-import { Button } from 'semantic-ui-react';
-import { Image } from 'semantic-ui-react';
-import { Icon } from 'semantic-ui-react';
+import { Container, Button, Image, Icon, Divider, Header, Segment, Grid, Rail } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <Container textAlign="justified">
-        <h1>Blog posts populares</h1>
-        <hr/>
-
-
+        <Header as='h1' textAlign='center' dividing>
+          Blog posts populares
+        </Header>
+        
         <div>
           <p>Orden: </p>
-          <Button content='Acendente' primary />
+          <Button content='Ascendente' primary inline />
+          <Button basic color='blue'>Ascendente</Button>
           <Button content='Descendente' primary />
         </div>
-        <div>
-          <Image src='/images/wireframe/image.png' size='small' />
-        </div>
 
-        <Icon name='angle up' />
-        <p>Votos</p>
-        <Icon name='angle down' />
+        <Grid centered columns={3}>
+          <Grid.Column width={6}>
+            <Image src='http://blog.makeitreal.camp/assets/images/bg-images/bundler.jpg' size='large' />
+          </Grid.Column>
+          <Grid.Column textAlign='center' width={1}>
+            <Icon name='angle up' />
+            <p>42</p>
+            <Icon name='angle down' />
 
-        <Container>
-          <h2>Descubre si Make it es</h2>
-        </Container>
-
-
+          </Grid.Column>
+          <Grid.Column width={9}>
+            <h3>Manejo de dependencias en Ruby con Bundler</h3>
+            <p>Bundler es una manejador de dependencias para Ruby. Aunque viene incluido con Rails, Bundler no es exclusivo de Rails, lo puedes usar para manejar las dependencias de cualquier proyecto de Ruby.</p>
+            <p>Escrito por: <Image src='//a.disquscdn.com/uploads/users/2864/1155/avatar92.jpg?1481303405' size='mini' circular inline /></p>
+          </Grid.Column>
+        </Grid>
       </Container>
 
     );
