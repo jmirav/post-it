2439763f34e8fb2df02401893ecdf83933a5f00f
jmirav <jmira420@gmail.com>
2018-08-16 23:32:23 -0500

    Implements preliminar function for listing posts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import posts from './posts'
 import { Container, Button, Image, Icon, Header, Grid } from 'semantic-ui-react';
 
 class App extends Component {
+  constructor() {
+    super();
+  }
+
+  renderPost(post) {
+    return (
+      <Grid centered columns={3} >
+        <Grid.Column width={6} className="post">
+          <Image src={post.post_image_url} size='large' />
+        </Grid.Column>
+        <Grid.Column textAlign='center' width={2} className="post" verticalAlign="middle">
+          <Icon name='angle up' />
+          <Header as='h3'>{post.votes}</Header>
+          <Icon name='angle down' />
+
+        </Grid.Column>
+        <Grid.Column width={8} className="post">
+          <a href={post.url} target='_blank' rel="noopener noreferrer">
+          <Header as='h3' color='blue'>
+            {post.title}
+          </Header>
+        </a>
+        <p>{post.description}</p>
+        <p className="author">Escrito por: <Image src={post.writer_avatar_url} size='mini' circular inline /></p>
+      </Grid.Column>
+    </Grid>
+    )
+  }
   render() {
     return (
       <Container textAlign="justified">
@@ -20,26 +49,10 @@ class App extends Component {
           <Button content='Descendente' primary />
         </div>
 
-        <Grid centered columns={3} >
-          <Grid.Column width={6} className="post">
-            <Image src='http://blog.makeitreal.camp/assets/images/bg-images/bundler.jpg' size='large' />
-          </Grid.Column>
-          <Grid.Column textAlign='center' width={1} className="post" verticalAlign="middle">
-            <Icon name='angle up' />
-            <Header as='h3'>42</Header>
-            <Icon name='angle down' />
+        <ul>
+          {posts.map(this.renderPost)}
+        </ul>
 
-          </Grid.Column>
-          <Grid.Column width={9} className="post">
-            <a href="http://blog.makeitreal.camp/manejo-de-dependencias-en-ruby-con-bundler/" target='_blank' rel="noopener noreferrer">
-              <Header as='h3' color='blue'>
-                Manejo de dependencias en Ruby con Bundler
-              </Header>
-            </a>
-            <p>Bundler es una manejador de dependencias para Ruby. Aunque viene incluido con Rails, Bundler no es exclusivo de Rails, lo puedes usar para manejar las dependencias de cualquier proyecto de Ruby.</p>
-            <p className="author">Escrito por: <Image src='//a.disquscdn.com/uploads/users/2864/1155/avatar92.jpg?1481303405' size='mini' circular inline /></p>
-          </Grid.Column>
-        </Grid>
       </Container>
 
     );
